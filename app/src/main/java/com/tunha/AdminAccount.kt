f568e26495d68b1d98fabd2c97286e39461f05d6
patrick <ngupatgi@gmail.com>
2023-03-11 19:31:43 +0300

    commits on Sat Mar 11 07:31:43 PM EAT 2023

--- app/src/main/java/com/tunha/AdminAccount.kt
+++ app/src/main/java/com/tunha/AdminAccount.kt
@@ -6,23 +6,44 @@ import android.content.ClipData.Item
 import android.content.ContentValues.TAG
 import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
 import android.nfc.Tag
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.transition.TransitionManager
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import com.tunha.databinding.ActivityAdminAccountBinding
+import kotlinx.coroutines.runBlocking
 import java.util.concurrent.CompletableFuture
+import kotlin.coroutines.resume
+import android.graphics.drawable.BitmapDrawable
+
 
 class AdminAccount : AppCompatActivity() {
     private var user: User? = null
@@ -37,7 +58,7 @@ class AdminAccount : AppCompatActivity() {
 
         var ids:String=Session.getUserSession(this).first.toString()
         Log.d(TAG,"User was $ids")
-        user=Session.getUser(ids)
+
 
 
 
@@ -65,37 +86,15 @@ class AdminAccount : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
+    val context=this
 
-
-        //run {
-            //val navigationView = findViewById<NavigationView>(R.id.nav_view)
-            //val profileMenuItem = navigationView.menu.findItem(R.id.navigation_profile)
-
-            val imageView = ImageView(this)
-            Log.d(TAG, "User is  $user", null)
-
-//            try{
-                val navigationView = findViewById<BottomNavigationView>(R.id.nav_view)
-                val profileMenuItem = navigationView.menu.findItem(R.id.navigation_profile)
-
-                user?.getProfileImage(this, imageView)
-
-                imageView.drawable?.let {
-                    Log.d(TAG,"found")
-                    profileMenuItem.icon = it
-                }
-//            }catch (exception:Exception)
-//            {
-//                exception.printStackTrace()
-//            }
-
-
-       // }
+        setUser()
+        //setDp()
 
     }
 
     override fun onBackPressed() {
-        preventBackPressed("Are you sure you want to exit the app?")
+        preventBackPressed("Are you sure you want to Logout from the app?")
     }
 
 
@@ -123,21 +122,124 @@ class AdminAccount : AppCompatActivity() {
 
         alertDialog.show()
     }
+
+    fun setDp(){
+        val context=this
+        val imageView=ImageView(context)
+
+        val navigationView = findViewById<BottomNavigationView>(R.id.nav_view)
+        val profileMenuItem = navigationView.menu.findItem(R.id.navigation_profile)
+
+        //setUser()
+
+
+
+
+
+
+        val storageRef = FirebaseStorage.getInstance().reference
+        var imagesRef = storageRef.child("profileImages/"+"user${user?.getId()}.jpg")
+        //imagesRef.child("user${user?.getId()}.jpeg")
+        //imagesRef=storageRef.child("user${user?.getId()}.jpg")
+       // Log.d(TAG,"/user${user?.getId()}.jpg")
+        imagesRef.downloadUrl.addOnSuccessListener { uri ->
+            Glide.with(context)
+                .load(uri)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(object : CustomTarget<Drawable>() {
+                    override fun onResourceReady(
+                        resource: Drawable,
+                        glideTransition: Transition<in Drawable>?
+                    ) {
+                        profileMenuItem.icon=resource
+                    }
+
+                    override fun onLoadCleared(placeholder: Drawable?) {
+                        // do nothing
+                    }
+                })
+        }.addOnFailureListener {
+            // if the user's profile image doesn't exist, load the default image from Firebase Storage
+            //val defaultImageRef = storageRef.child("defaultprofile.jpg")
+            val defaultImageRef=storageRef.child("user1.jpg")
+            defaultImageRef.downloadUrl.addOnSuccessListener { uri ->
+                Glide.with(context)
+                    .load(uri)
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .into(object : CustomTarget<Drawable>() {
+                        override fun onResourceReady(
+                            resource: Drawable,
+                            glideTransition: Transition<in Drawable>?
+                        ) {
+
+                            profileMenuItem.icon=resource
+                        }
+
+                        override fun onLoadCleared(placeholder: Drawable?) {
+                            // do nothing
+                        }
+                    })
+            }.addOnFailureListener {
+                Toast.makeText(context, "Failed to get image", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    fun setUser(){
+
+        val auth = FirebaseAuth.getInstance()
+
+        auth.signInAnonymously()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // User is signed in anonymously
+                    Log.d(TAG,"Sucess")
+                } else {
+                    // Sign in failed
+                    Log.d(TAG,"Auth failed")
+                }
+            }
+
+
+
+
+        var ids:String=Session.getUserSession(this).first.toString()
+        val database = Firebase.database
+        val myRef = database.getReference("users")
+
+        // Query data where the "userId" is equal to the given userId parameter
+        val query = myRef.child(ids)
+
+        query.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                user = if (dataSnapshot.exists()) {
+                    dataSnapshot.getValue(User::class.java)
+                } else {
+                    null
+                }
+                if (dataSnapshot.exists()) {
+                    Log.d(TAG,"\n\nuser  \n\n")
+                    Log.d(TAG, user.toString())
+                    //setDp()
+                }
+                else
+                {
+                    Log.d(TAG,"\n\nFailed to see user\n\n")
+                }
+                //continuation.resume(user)
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.e(TAG, "Error getting user with id $ids: ${databaseError.message}")
+                //continuation.resume(null)
+            }
+        })
+    }
+
+
+
+
 }
 
-private fun <T> CompletableFuture<T>.get(onError: () -> Unit?): User? {
-    try {
-        // Get the result of the CompletableFuture
-        val result = this.get()
-        // If the result is a User, return it
-        if (result is User) {
-            return result
-        }
-    } catch (e: Exception) {
-        // If the CompletableFuture throws an exception, call the onError lambda
-        onError?.invoke()
-    }
-    // If the result is not a User or the CompletableFuture fails, return null
-    return null
-}
+
 
--- app/src/main/java/com/tunha/ui/admins/Admins.kt
+++ app/src/main/java/com/tunha/ui/admins/Admins.kt
@@ -1,11 +1,24 @@
 package com.tunha.ui.admins
 
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.text.Editable
+import android.util.Log
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
+import androidx.core.content.ContextCompat
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.tunha.*
 import com.tunha.R
 
 class Admins : Fragment() {
@@ -20,13 +33,166 @@ class Admins : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_admins, container, false)
+        //return inflater.inflate(R.layout.fragment_admins, container, false)
+
+        // Inflate the fragment layout
+        val rootView = inflater.inflate(R.layout.fragment_admins, container, false)
+
+        val button = rootView.findViewById<Button>(R.id.addAdmin)
+
+        // Set an OnClickListener on the button to handle clicks
+        button.setOnClickListener {
+            // Create an Intent to start the other activity
+            val intent = Intent(activity, AddAdmin::class.java)
+
+            // Start the activity
+            activity?.startActivity(intent)
+        }
+
+        val sach=rootView.findViewById<EditText>(R.id.sach)
+        feedData(rootView,sach.text.toString())
+
+        val dosearch=rootView.findViewById<ImageView>(R.id.dosearch)
+
+        dosearch.setOnClickListener(View.OnClickListener {
+            feedData(rootView,sach.text.toString())
+        })
+
+        return rootView
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(AdminsViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    fun feedData(view: View, sach: String){
+        // Get a reference to the database node that contains the user information
+        //val databaseRef = FirebaseDatabase.getInstance().getReference("users")
+
+        // Get a reference to the parent LinearLayout that will hold the user items
+        val parentLinearLayout = view.findViewById<LinearLayout>(R.id.holdData)
+
+        val database = Firebase.database
+        val myRef = database.getReference("users")
+
+// Query data where the "email" field is equal to "test@example.com"
+        var query = myRef.orderByChild("id").startAt("0").endAt("\uf8ff")
+//            .equalTo("usertype","Admin")
+//                if(sach.isNotEmpty()) {
+//                    query = myRef.orderByChild("id").startAt("0").endAt("\uf8ff").orderByChild("usertype").equalTo("Admin")
+//                        .orderByChild("email")
+//                        .startAt(sach)
+//                        .endAt(sach + "\uf8ff")
+//                        .orderByChild("name")
+//                        .startAt(sach)
+//                        .endAt(sach + "\uf8ff")
+//                }
+
+        query.addValueEventListener(object :
+            ValueEventListener {
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                Log.d(TAG, "onDataChange: ")
+                parentLinearLayout.removeAllViews()
+                for (childSnapshot in dataSnapshot.children.reversed()) {
+                    Log.d(TAG, "reading dta")
+                    // Print the data for each child node
+                    println("${childSnapshot.key} => ${childSnapshot.value}")
+                    val us: User? = childSnapshot.getValue(User::class.java)
+
+
+                    if (us != null) {
+                        if(sach.isEmpty()||us.getEmail().contains(sach)||us.getFullName().contains(sach))
+                             {
+                                if(us.getUserType()=="Admin")
+                                    parentLinearLayout.addView(us?.let { feedUser(it) })
+                            }
+                    }
+
+
+
+                }
+
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+
+    }
+
+    fun feedUser(user: User):LinearLayout
+    {
+        val parentLinearLayout = LinearLayout(context)
+        parentLinearLayout.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        parentLinearLayout.orientation = LinearLayout.VERTICAL
+        parentLinearLayout.setPadding(20, 20, 20, 20)
+
+        val childLinearLayout = LinearLayout(context)
+        childLinearLayout.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        childLinearLayout.orientation = LinearLayout.HORIZONTAL
+        childLinearLayout.setPadding(16, 16, 16, 16)
+        childLinearLayout.elevation = 4f
+        //childLinearLayout.outlineProvider = BackgroundOutlineProvider()
+        childLinearLayout.background = ColorDrawable(Color.parseColor("#FAFAFA"))
+
+        val imageView = ImageView(context)
+        imageView.layoutParams = LinearLayout.LayoutParams(
+            100,
+            LinearLayout.LayoutParams.MATCH_PARENT
+        )
+        imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+        imageView.setImageResource(R.drawable.iconnny)
+        imageView.foregroundGravity=Gravity.CENTER
+        context?.let { user.getProfileImage(it, imageView) }
+        imageView.setPadding(0, 0, 12, 0)
+        imageView.clipToOutline = true
+        imageView.background = context?.let { ContextCompat.getDrawable(it, R.drawable.rounded_corner_background) }
+        imageView.setPadding(20,18,20,18)
+
+
+        val textLinearLayout = LinearLayout(context)
+        textLinearLayout.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        textLinearLayout.orientation = LinearLayout.VERTICAL
+        textLinearLayout.setPadding(50, 0, 0, 0)
+
+        val nameTextView = TextView(context)
+        nameTextView.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        nameTextView.text = user.getFullName()
+
+        val emailTextView = TextView(context)
+        emailTextView.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        emailTextView.text = user.getEmail();
+
+        textLinearLayout.addView(nameTextView)
+        textLinearLayout.addView(emailTextView)
+
+        childLinearLayout.addView(imageView)
+        childLinearLayout.addView(textLinearLayout)
+
+        parentLinearLayout.addView(childLinearLayout)
+
+        return parentLinearLayout
+
     }
 
 }
